178dd1d27d3bda3531d066fa49447c4c9561a452
Esteban Escalante <escalanteaveleyra@gmail.com>
2024-07-23 14:41:29 +0200

    Solved lab

---
+++ src/components/User.jsx
@@ -0,0 +1,13 @@
+function User({name, handle}){
+    return(
+        <>
+        <span className="name">{name}</span>
+        <span className="handle">@{handle}</span>
+        </>
+    );
+    }
+    export default User;
+
+
+
+    
